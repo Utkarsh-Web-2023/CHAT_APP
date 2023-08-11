1678b8e98af689ad382298e8809e1577b448c5f8
Utkarsh Web <128823414+Utkarsh-Web-2023@users.noreply.github.com>
2023-08-11 12:56:31 +0530

    first commit

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const serverless = require('serverless-http')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const path = require('path');  // Add path module
+const router = express.Router()
+// const server = http.createServer(app);  // Create server using http module
+// const io = socketIO(server);
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));  // Use path.join for correct file path
+});
+
+app.use(express.static(path.join(__dirname, '../dist')));  // Use path.join for correct static content path
+
+const systemGreeting = {
+    user: 'Jarvis',
+    message: 'Welcome to Utkarsh Chat APP'
+};
+
+io.on('connection', (socket) => {
+    socket.emit('message', systemGreeting);  // Send a welcome message to the connecting client
+    console.log('Connected...');
+
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg);  // Broadcast the received message to all clients
+    });
+
+    socket.on('joined', (name) => {
+        const systemMessage = {
+            user: 'Jarvis',
+            message: `${name} has joined the chat.`
+        };
+        
+        io.emit('message', systemMessage);  // Broadcast the join message to all clients
+    });
+})
+app.use('/.netlify/functions/index', router)
+module.exports.handler = serverless(app);
+http.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
