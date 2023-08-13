3c98b138da8a9c2ec1e2ac4e6f4976669027457e
Utkarsh Web <128823414+Utkarsh-Web-2023@users.noreply.github.com>
2023-08-13 11:23:32 +0530

    New UPDATE OUT

--- functions/index.js
+++ functions/index.js
@@ -43,3 +43,25 @@ module.exports.handler = serverless(app);
 http.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
   })
+  // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+const firebase = require('firebase/app');
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1a3IRwzMlVJiU_qznTesBqFk6p0t4Kfs",
+  authDomain: "chat-app-utkarsh.firebaseapp.com",
+  projectId: "chat-app-utkarsh",
+  storageBucket: "chat-app-utkarsh.appspot.com",
+  messagingSenderId: "37737525277",
+  appId: "1:37737525277:web:94fde5a27b2f6c62a796f4",
+  measurementId: "G-XRBVT6S1QJ"
+};
+
+// // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
